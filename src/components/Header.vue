fc7a39e0d9f1d63c082d7c78758736074ac1a0ca
unknown <GRUPOREINCAR@COMER-XIOMARA.grreincar.local>
2023-02-08 17:07:10 -0500

    Changes

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,17 +1,38 @@
 <template>
-  <body>
-    
- 
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  </head>
+ <div class="nav-bar">
   <nav class="navbar navbar-light nav-pills">
     <!-- <div class="container"> -->
       <ul class="nav">
        
         <li class="nav-item">
-          <router-link to="/Index" class="nav-link" active-class="active"
+          <router-link to="/Header" class="nav-link" active-class="active"
             >Inicio</router-link
           >
         </li>
-        <li class="nav-item" v-if="verificar">
+
+        <li class="nav-item">
+          <RouterLink to="" class="nav-link">Recursos Humanos</RouterLink>
+        <ul>
+          <li class="submenu"><RouterLink to="">¿Como acceder al beneficio de gafas?</RouterLink></li>
+          <li class="submenu"><RouterLink to="">¿Como solicitar un certificado laboral?</RouterLink></li>
+          <li class="submenu"><RouterLink to="">¿Como solicitar un permiso o informar una calamidad?</RouterLink></li>
+        <!--   <li class="submenu"><RouterLink to="" class="sublist"></RouterLink></li>
+          <li class="submenu"><RouterLink to="" class="sublist"></RouterLink></li>
+          <li class="submenu"><RouterLink to="" class="sublist"></RouterLink></li>
+          <li class="submenu"><RouterLink to="" class="sublist"></RouterLink></li> -->
+        </ul>
+        </li>
+      </ul> 
+
+      <h1>Bienvenido!</h1>
+      </nav>
+</div>
+       <!--  <li class="nav-item" v-if="verificar">
           <router-link to="/FormData" class="nav-link" active-class="active"
             >Agregar Usuarios</router-link
           >
@@ -30,22 +51,23 @@
           <router-link to="/LoginUno" class="nav-link" active-class="active"
             >Inicio de Sesion</router-link
           >
-        </li> 
+        </li>  -->
         <!-- <li class="nav-item">
           <router-link to="/Productos" class="nav-link" active-class="active"
             >Productos</router-link
           >
         </li> -->
-        <li class="nav-item" v-if="verificar">
+        <!-- <li class="nav-item" v-if="verificar">
           <router-link to="/PedidoGet" class="nav-link" active-class="active"
             >PedidoGet</router-link
           >
-        </li>
-        <button v-if="verificar"><a href="/log">Cerrar Sesion</a></button>
-      </ul>
-      <h1 v-if="verificar">Bienvenido!</h1>
-  </nav>
-</body>
+        </li> -->
+        <!-- <button v-if="verificar"><a href="/log">Cerrar Sesion</a></button>
+        --> 
+     
+   
+
+    
 </template>
 <script>
 
@@ -71,28 +93,57 @@ export default {
 }}
 </script>
 <style>
+.submenu{
+  position: absolute;
+  background: #333333;
+  width: 120%;
+  display:none;
+}
+.submenu li{
+  display: block;
+  padding: 20px;
+  color: #ffffff;
+  font-family: 'Times New Roman', Times, serif;
+  text-decoration: none;
+ 
+}
+.nav-item li:hover .submenu{
+  display: block;
+}
+div.nav-bar{
+  display: block;
+  margin:auto;
+  margin-top: 10px;
+}
 body{
-  background-color: #c2aa9e;
+  background-color: #204ece;
+  margin-top:-20px;
 }
 .nav-item{
   color: #8b6c53;
+  display: block;
+  padding:15px 20px;
+  text-decoration: none;
 }
 .nav-link{
   color: #FFF;
   background-color:#8e400000;
   border-color: #95786b;
   transition-duration: 0.4s;
-
 }
 .nav-link:hover {
-  background-color: #af9784;
+  background-color: #547fdb;
   color: white;
 }
 .nav-pills .nav-link.active, .nav-pills .show>.nav-link {
-    color: #fff;
-    background-color: #9f8672;
+    color: #000000;
+    background-color: #ffffff;
 }
 .navbar{
   color:#8b6c53
+}
+h1{
+  font-family: 'Times New Roman', Times, serif;
+  color:#ffffff;
 }
 </style>
--- src/routes.js
+++ src/routes.js
@@ -1,23 +1,18 @@
 import LoginUno from './components/LoginUno.vue'
-import FormData from './pages/FormData.vue'
+
 import PrintData from './pages/PrintData.vue'
-import Details from './pages/Details.vue'
-import Index from './pages/ProductosCard.vue'
 import Pedidos from './pages/RegistroPedido.vue'
 import PedidoGet from './pages/ImprimirPedido.vue'
 import InfoPedido from './pages/InfoPedido.vue'
-
+import AppVue from './App.vue'
 
 
 export const routes = [
     {path:'/LoginUno',component:LoginUno},
-    {path:'/FormData', component:FormData},
     {path:'/print', component:PrintData},
-    {path:'/details/:index', component:Details},
-    {path:'/Index', component:Index},
     {path:'/log', component:LoginUno},
     {path:'/PedidoGet', component:PedidoGet},
     {path:'/Pedidos',component:Pedidos},
-    {path:'/InfoPedido/:index',component:InfoPedido}
-
+    {path:'/InfoPedido/:index',component:InfoPedido},
+    {path:'/AppVue',component:AppVue}
 ]
